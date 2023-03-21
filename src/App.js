c6cc44c8506b91b9258bd53a63bfb7931968c17d
md shahanwaz siddique <mdshahanwazsiddique6@gmail.com>
2023-03-21 22:14:48 +0530

    Add Data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Board from "./TicTacToeGame/Board";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display:"flex",flexDirection:"column", justifyContent:"center", alignItems:"center",background:"#de9967",height:"100vh"}}>
+      <h1>Tic Tac Toe Game</h1>
+    <Board/>
     </div>
   );
 }
